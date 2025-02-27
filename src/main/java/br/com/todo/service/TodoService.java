99a9bfa5b3d690deef7321f1e75230d3416e7fd2
Arthur Selingin <arthur_santos@outlook.com.br>
2025-02-26 23:56:35 -0300

    formatação

--- src/main/java/br/com/todo/service/TodoService.java
+++ src/main/java/br/com/todo/service/TodoService.java
@@ -10,29 +10,37 @@ import br.com.todo.repository.TodoRepository;
 
 @Service
 public class TodoService {
-     private TodoRepository todoRepository;
-     public TodoService(TodoRepository todoRepository) {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
-    
-    public List<Todo> list(){
-        Sort sort =  Sort.by("prioridade").descending().and(
-              Sort.by("title").ascending());
-          return  todoRepository.findAll(sort);
-      }
 
-    public List<Todo> create(Todo todo){
-        todoRepository.save(todo);  
+    public List<Todo> list() {
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("title").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
         return list();
     }
 
-     public List<Todo> update(Todo todo){
-        todoRepository.save(todo); 
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
         return list();
     }
-    
-     public List<Todo> delete(Long id){
-        todoRepository.deleteById(id);
-        return list();
-     }
+
+    public Todo findById(Long id) {
+        return todoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Todo não encontrado"));
+    }
+
+    public List<Todo> delete(Long id) {
+       Todo todo =  findById(id);
+        todoRepository.delete(todo);
+        return list(); 
+        }
+
 }
